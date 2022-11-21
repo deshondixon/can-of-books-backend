d6ed10bece0597ce47f0118e7c3e96f7829fac8d
deshondixon <deshondixon@gmail.com>
2022-11-21 14:25:08 -0800

    minor fix

--- seed.js
+++ seed.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Book = require('./models/books');
 mongoose.connect(process.env.DB_URL);
 
-
 async function seed() {
-//   const myBook = new Book({
-//     title: 'Lord of The Rings',
-//     description: 'An epic saga that takes place in the past.',
-//     status: false,
-//   });
-//   myBook.save(function (err) {
-//     if (err) console.error(err);
-//     else console.log('');
-//   });
+  const myBook = new Book({
+    title: 'Lord of The Rings',
+    description: 'An epic saga that takes place in the past.',
+    status: false,
+  });
+  myBook.save(function (err) {
+    if (err) console.error(err);
+    else console.log('');
+  });
 
   await Book.create({
     title: 'Hatchet',
